6f991d449ebab904164646d8e60fc9639bb60500
Jan Domanski <jandom@gmail.com>
2017-06-03 18:40:43 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='DSPP-Keras',
-      version='0.0.2',
+      version='0.0.3',
       description='Integration of DSPP database with Keral Machine Learning Library',
       author='Jan Domanski',
       author_email='jan@peptone.io',
       url='https://github.com/PeptoneInc/dspp-keras',
-      download_url='https://github.com/PeptoneInc/dspp-keras/archive/v0.0.1.tar.gz',
+      download_url='https://github.com/PeptoneInc/dspp-keras/archive/v0.0.3.tar.gz',
       license='MIT',
       install_requires=['keras', 'numpy'],
       packages=find_packages())
