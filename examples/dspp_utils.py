e26629549d9b7aa9752fe2b83ce8e15e2e629b3f
Kamil Tamiola <fora@tamiola.com>
2017-06-05 15:11:01 +0200

    Add basic support for multi-gpu training

--- examples/dspp_utils.py
+++ examples/dspp_utils.py
@@ -4,9 +4,13 @@ Peptone Inc. - The Protein Intelligence Company (https://peptone.io)
 '''
 
 from keras.callbacks import Callback
+from keras.layers import merge
+from keras.layers.core import Lambda
+from keras.models import Model
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+
 
 class lossRatio(Callback):
     """
@@ -98,3 +102,49 @@ def chi2(exp, obs):
     name="chi2_statistics")
 
     return stat
+
+
+def make_parallel(model, gpu_count):
+    """
+        A plain and naive multi-GPU implementation.
+        Based on: https://github.com/kuza55/keras-extras/blob/master/utils/multi_gpu.py
+    """
+    def get_slice(data, idx, parts):
+        shape = tf.shape(data)
+        size = tf.concat([ shape[:1] // parts, shape[1:] ],axis=0)
+        stride = tf.concat([ shape[:1] // parts, shape[1:]*0 ],axis=0)
+        start = stride * idx
+        return tf.slice(data, start, size)
+
+    outputs_all = []
+    for i in range(len(model.outputs)):
+        outputs_all.append([])
+
+    #Place a copy of the model on each GPU, each getting a slice of the batch
+    for i in range(gpu_count):
+        with tf.device('/gpu:%d' % i):
+            with tf.name_scope('tower_%d' % i) as scope:
+
+                inputs = []
+                #Slice each input into a piece for processing on this GPU
+                for x in model.inputs:
+                    input_shape = tuple(x.get_shape().as_list())[1:]
+                    slice_n = Lambda(get_slice, output_shape=input_shape, arguments={'idx':i,'parts':gpu_count})(x)
+                    inputs.append(slice_n)
+
+                outputs = model(inputs)
+
+                if not isinstance(outputs, list):
+                    outputs = [outputs]
+
+                #Save all the outputs for merging back together later
+                for l in range(len(outputs)):
+                    outputs_all[l].append(outputs[l])
+
+    # merge outputs on CPU
+    with tf.device('/cpu:0'):
+        merged = []
+        for outputs in outputs_all:
+            merged.append(merge(outputs, mode='concat', concat_axis=0))
+
+        return Model(input=model.inputs, output=merged)
