e45b3708755553a676167c1273d72e3609c9681a
Jan Domanski <jandom@gmail.com>
2017-06-10 19:38:24 +0100

    Final cleanups and linting
    - bring back sample weights
    - plotting changes

--- examples/plot.py
+++ examples/plot.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python
+
+'''
+Utilities for convnet on the dSPP (https://peptone.io/dssp) dataset.
+Peptone Inc. - The Protein Intelligence Company (https://peptone.io)
+'''
+from __future__ import print_function
+
 from keras.models import model_from_json
-from dspp_train import X, Y, weights
-from dsppkeras.datasets import dspp
-import matplotlib, os
+from train import X, Y, weights
+import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
@@ -20,13 +27,12 @@ prediction = model.predict(X[::100])
 # plot the results
 print("Plotting...")
 fig = plt.figure(figsize=(9*3, 9*3))
-for i, (exp, w, pred) in enumerate(zip(Y[::100], weights[::100], prediction)):
-    ax = fig.add_subplot(9,9,i+1)
+for i, (exp, weight, pred) in enumerate(zip(Y[::100], weights[::100], prediction)):
+    ax = fig.add_subplot(9, 9, i+1)
     ax.plot(exp, label="propensity")
     ax.plot(pred, label="prediction")
-    ax.plot(w, label="weights")
+    ax.plot(weight, label="weights")
     ax.legend()
-    ax.set_xlim(0,250)
 fig.tight_layout()
 
 fig.savefig("plot.png")
