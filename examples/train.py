286e4899c6a97ee8c8ec4f5f0dd91ed62763e8b7
Jan Domanski <jandom@gmail.com>
2017-06-10 19:38:24 +0100

    Final cleanups and linting
    - bring back sample weights
    - plotting changes

--- examples/train.py
+++ examples/train.py
@@ -1,74 +1,69 @@
+#!/usr/bin/env python
+
 '''
 Basic convnet on the dSPP (https://peptone.io/dssp) dataset.
 Peptone Inc. - The Protein Intelligence Company (https://peptone.io)
 '''
 
 from __future__ import print_function
-import keras, os, time
-from dsppkeras.datasets import dspp
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Reshape, Conv1D, MaxPooling1D, BatchNormalization, Activation
-from keras.losses import logcosh
-from utils import Struct, lettercode2onehot, shuffle_and_split, LossRatio
 
-def get_model(config):
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.losses import logcosh
+from keras.preprocessing.sequence import pad_sequences
+
+from dsppkeras.datasets import dspp
+from utils import Struct, lettercode2onehot, shuffle_and_split, generate_weights, LossRatio, rmsd, chi2
+
+def get_model(args):
     """
     Build model given some initial configuration
+
+    WARNING this is decoy model that serves no practical pourpose
+    The model has 12 mln trainable paramaters, which is more than the dataset...
+    Obviously it makes no sense and is shown just as a demo.
+    
     """
     model = Sequential()
-    model.add(Dense(config.N, input_shape=(config.N,), name="Prediction"))
+    model.add(Dense(args.maxlen, input_shape=(20*args.maxlen,), name="Prediction"))
+    print(model.summary())
     return model
 
 # all free parameters for the model
-args = {
-    "N": 800,
-}
-parameters = Struct(**args)
+parameters = Struct(**{
+    "maxlen": 800,
+})
 
 X, Y = dspp.load_data()
 weights = generate_weights(Y)
-
 X = [lettercode2onehot(x) for x in X]
-X = pad(X, 20*parameters.N)
-Y = pad(Y, parameters.N)
-weights = pad(weights, parameters.N)
+X = pad_sequences(X, 20*parameters.maxlen)
+Y = pad_sequences(Y, parameters.maxlen, dtype='float32')
+weights = pad_sequences(weights, parameters.maxlen, dtype='float32')
 
 if __name__ == '__main__':
 
     # Shuffle and split the data
     (x_train, y_train, weights_train), (x_test, y_test, weights_test) = shuffle_and_split(X, Y, weights)
 
-    batch_size = 8000
+    batch_size = 128
     epochs = 10
 
-    # Measure time
-    tic = time.time()
-
     model = get_model(parameters)
-    model.compile(optimizer=keras.optimizers.Adam(), loss=logcosh, metrics=[rmsd, chi2], sample_weight=weights_train, sample_weight_mode="temporal")
+    model.compile(optimizer=keras.optimizers.Adam(), loss=logcosh, metrics=[rmsd, chi2], sample_weight=weights_train)
 
-    model.fit(x=x_train, y=y_train, epochs=epochs, batch_size=batch_size, validation_data=(x_test, y_test, weights_test), callbacks=[RossRatio()])
-    score = model.evaluate(x_test, y_test, sample_weight=weights_test)
-
-    # Measure time
-    toc = time.time()
+    model.fit(x=x_train, y=y_train, epochs=epochs, batch_size=batch_size,
+              validation_data=(x_test, y_test), callbacks=[LossRatio()])
+    score = model.evaluate(x_test, y_test)
 
     # Just some simple diagnostics
-    print()
     print('Test rmsd:', score[0])
     print('Test chi2:', score[1])
 
-    # Output time
-    print('Training time {}h {}m {}s'.format(timeit(toc-tic)[0], timeit(toc-tic)[1], timeit(toc-tic)[2]))
-
-    # Make sure we have output directory
-    if not os.path.exists("./model"):
-        os.makedirs("./model")
-
     # Serialize model to JSON
-    with open("./model/model.json", "w") as fp:
+    with open("model.json", "w") as fp:
         fp.write(model.to_json())
 
     # Serialize weights to HDF5
-    model.save_weights("./model/model.h5")
-    print("Saved model to ./model/")
+    model.save_weights("model.h5")
--- examples/utils.py
+++ examples/utils.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python
+
 '''
 Utilities for convnet on the dSPP (https://peptone.io/dssp) dataset.
 Peptone Inc. - The Protein Intelligence Company (https://peptone.io)
 '''
+from __future__ import print_function
 
 from keras.callbacks import Callback
-from keras.layers import merge
-from keras.layers.core import Lambda
-from keras.models import Model
 import numpy as np
 import tensorflow as tf
 
@@ -26,34 +26,17 @@ def lettercode2onehot(sequence):
     """
         Return a binary one-hot vector
     """
-    one_digit = {'A': 0, 'C': 1, 'D': 2, 'E': 3, 'F': 4,'G': 5,
-    'H': 6, 'I': 7, 'K': 8, 'L': 9, 'M': 10, 'N': 11,'P': 12,
-     'Q': 13, 'R': 14, 'S': 15, 'T': 16, 'V': 17,'W': 18, 'Y': 19}
+    one_digit = {'A': 0, 'C': 1, 'D': 2, 'E': 3, 'F': 4, 'G': 5, \
+        'H': 6, 'I': 7, 'K': 8, 'L': 9, 'M': 10, 'N': 11, 'P': 12, \
+        'Q': 13, 'R': 14, 'S': 15, 'T': 16, 'V': 17, 'W': 18, 'Y': 19}
 
-    assert(len(sequence) >= 1)
+    assert len(sequence) >= 1
     encoded = []
     for letter in sequence:
         tmp = np.zeros(20)
         tmp[one_digit[letter]] = 1
         encoded.append(tmp)
-    assert(len(encoded) == len(sequence))
-    encoded = np.asarray(encoded)
-    return list(encoded.flatten())
-
-def lettercode2number(sequence):
-    """
-        Return an integer vector with amino acid identities, shifted by +1.
-        We want to make sure 0 is used for padding and missing data.
-    """
-    one_digit = {'A': 0, 'C': 1, 'D': 2, 'E': 3, 'F': 4,'G': 5,
-    'H': 6, 'I': 7, 'K': 8, 'L': 9, 'M': 10, 'N': 11,'P': 12,
-     'Q': 13, 'R': 14, 'S': 15, 'T': 16, 'V': 17,'W': 18, 'Y': 19, 'X': 20}
-
-    assert(len(sequence) >= 1)
-    encoded = []
-    for letter in sequence:
-        encoded.append(one_digit[letter]+1)
-    assert(len(encoded) == len(sequence))
+    assert len(encoded) == len(sequence)
     encoded = np.asarray(encoded)
     return list(encoded.flatten())
 
@@ -67,24 +50,17 @@ def generate_weights(array):
     weights = np.array(results)
     return weights
 
-def normalize(array):
-    concatenated = np.concatenate(array)
-    mean, std = concatenated.mean(), concatenated.std()
-    return [ (row - mean)/std for row in array ]
-
 def shuffle_and_split(X, Y, weights, seed=123456, fraction=0.8):
-    assert(X.shape[0]==Y.shape[0])
-    assert(X.shape[0]==weights.shape[0])
+    assert X.shape[0] == Y.shape[0]
+    assert X.shape[0] == weights.shape[0]
     N = X.shape[0]
     np.random.seed(seed)
     indices = np.random.permutation(N)
     idx = int(N*fraction)
     training_idx, test_idx = indices[:idx], indices[idx:]
+
     (x_train, y_train, weights_train) = (X[training_idx], Y[training_idx], weights[training_idx])
     (x_test, y_test, weights_test) = (X[test_idx], Y[test_idx], weights[test_idx])
-
-    y_test = np.reshape(y_test, y_test.shape + (1,))
-    y_train = np.reshape(y_train, y_train.shape + (1,))
 
     return (x_train, y_train, weights_train), (x_test, y_test, weights_test)
 
@@ -101,76 +77,15 @@ def chi2(exp, obs):
     zero = tf.constant(0, dtype=tf.float32)
     mask = tf.not_equal(exp, zero)
 
-    def foo(tensor, mask):
+    def masking(tensor, mask):
         return tf.boolean_mask(tensor, mask)
 
     stat = tf.reduce_sum(
         tf.div(
             tf.pow(
-                tf.subtract(foo(obs, mask),foo(exp, mask)),
-            2),
-        foo(exp, mask)),
-    name="chi2_statistics")
+                tf.subtract(masking(obs, mask), masking(exp, mask)),
+                2),
+            masking(exp, mask)),
+        name="chi2_statistics")
 
     return stat
-
-def timeit(t):
-    """
-        Compute time in h,m,s format for performance logging
-    """
-    m, s = divmod(t, 60)
-    h, m = divmod(m, 60)
-    return int(h),int(m),int(s)
-
-def make_parallel(model, gpu_count):
-    """
-        A plain and naive multi-GPU implementation.
-        Based on: https://github.com/kuza55/keras-extras/blob/master/utils/multi_gpu.py
-    """
-
-    def get_slice(data, idx, parts):
-
-        # This is a necessary work-around the issue related
-        # to serialization of multi-GPU model in Keras
-        # without tensorflow import declaration we won't
-        # be able to predict back the values from a multi-GPU model
-        import tensorflow as tf
-
-        shape = tf.shape(data)
-        size = tf.concat([ shape[:1] // parts, shape[1:] ],axis=0)
-        stride = tf.concat([ shape[:1] // parts, shape[1:]*0 ],axis=0)
-        start = stride * idx
-        return tf.slice(data, start, size)
-
-    outputs_all = []
-    for i in range(len(model.outputs)):
-        outputs_all.append([])
-
-    #Place a copy of the model on each GPU, each getting a slice of the batch
-    for i in range(gpu_count):
-        with tf.device('/gpu:%d' % i):
-            with tf.name_scope('tower_%d' % i) as scope:
-
-                inputs = []
-                #Slice each input into a piece for processing on this GPU
-                for x in model.inputs:
-                    input_shape = tuple(x.get_shape().as_list())[1:]
-                    slice_n = Lambda(get_slice, output_shape=input_shape, arguments={'idx':i,'parts':gpu_count})(x)
-                    inputs.append(slice_n)
-
-                outputs = model(inputs)
-
-                if not isinstance(outputs, list):
-                    outputs = [outputs]
-
-                #Save all the outputs for merging back together later
-                for l in range(len(outputs)):
-                    outputs_all[l].append(outputs[l])
-
-    # merge outputs on CPU
-    with tf.device('/cpu:0'):
-        merged = []
-        for outputs in outputs_all:
-            merged.append(merge(outputs, mode='concat', concat_axis=0))
-
-        return Model(input=model.inputs, output=merged)
